fbf63a1beb50c4b7b6151364a2b6d147ecca7c6a
SGK <55636018+SGKespace@users.noreply.github.com>
2023-05-01 01:20:46 +0400

    Проведена работа над ошибками

--- render_website.py
+++ render_website.py
@@ -1,12 +1,13 @@
 import json
-
+import argparse
+import sys
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
 from livereload import Server
 
 
-PAGE_DIRECTORY = 'pages'
+PAGES_DIRECTORY = 'pages'
 
 
 def on_reload():
@@ -14,36 +15,49 @@ def on_reload():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
-    template = env.get_template('./template.html')
-
-    books_path = Path.joinpath(Path.cwd().parents[0], 'lesson10/', 'category.json')
-    with open(books_path, 'r', encoding='utf-8') as json_file:
-        books = json.load(json_file)
-
+    parser = argparse.ArgumentParser(description='Сайт для чтения книг, скачанных с https://tululu.org/.')
+    parser.add_argument(
+        '--json_path',
+        nargs='?',
+        type=Path,
+        default='media/category.json',
+        help='путь и имя файла о книгах .json'
+    )
+    parser.add_argument(
+        '--books',
+        nargs='?',
+        type=int,
+        default=20,
+        help='количество книг на страницe'
+    )
+    parser_args = parser.parse_args()
+    try:
+        with open(parser_args.json_path, 'r', encoding='utf-8') as json_file:
+            books_description = json.load(json_file)
+    except FileNotFoundError as error:
+        sys.exit(f'Неверно указан путь или имя файлаю Ошибка {error}')
     book_form = []
-    for page in books:
+    for page in books_description:
         for book in page:
             book_form.insert(0, page[book])
-
-    Path(PAGE_DIRECTORY).mkdir(parents=True, exist_ok=True)
-    books = list(chunked(book_form, 20))
-    for page_number, books_page in enumerate(books):
+    template = env.get_template('./template.html')
+    Path(PAGES_DIRECTORY).mkdir(parents=True, exist_ok=True)
+    books_description = list(chunked(book_form, parser_args.books))
+    for page_number, books_page in enumerate(books_description, start=1):
         rendered_page = template.render(
-            books=books[page_number],
-            pages_number=len(books),
+            books_description=books_page,
+            pages_number=len(books_description),
             page_number=page_number
         )
-        with open(Path(PAGE_DIRECTORY).joinpath(f'index{page_number + 1}.html'), 'w', encoding="utf8") as file:
+        with open(Path(PAGES_DIRECTORY).joinpath(f'index{page_number}.html'), 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
 def main():
     on_reload()
-    server = Server()
-    server.watch('*.html', on_reload)
-    server.serve(root='.')
 
 
 if __name__ == '__main__':
     main()
+
+
